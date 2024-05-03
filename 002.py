bc41b15cc0a6a0be0334c7b29f1ca7db76a62f18
mcclara2408 <mariaclarafernandesdefarias@gmail.com>
2024-05-03 20:57:00 -0300

    Observações sobre o commit

---
+++ 002.py
@@ -0,0 +1,12 @@
+acai = str (input("como podemos ajudar?"))
+
+if acai =='quero montar meu açai.':
+    print("quais serão os complementos?")
+elif acai =='quais sao os complementos disponiveis':
+    print ("banana")
+    print ("uva")
+    print ("morango")
+    print ("granola")
+    print ("leite condensado")
+elif acai == 'morango, banana e leite condensado':
+    
